026228d9076c327b7633f60a2439bc86a44d9e9f
nishikantvijeta <128150871+nishikantvijeta@users.noreply.github.com>
2025-08-11 08:24:21 +0530

    Update auth.js

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -8,6 +8,7 @@ export default function (req, res, next) {
  // if (!authHeader) return res.status(401).json({ error: "No token" });
    if (!authHeader || !authHeader.startsWith("Bearer ")) {
   return res.status(401).json({ error: "No token provided" });
+   }
   const token = authHeader.split(" ")[1];
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
